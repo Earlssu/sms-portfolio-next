beafd4b626053ee489d03d3e4389b17780aee531
minseobshim <mshim@blast-team.com>
2024-08-27 22:57:53 +0900

    feat: 타이핑 텍스트 애니메이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import TypingText from "@/components/TypingText";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HELLO WORK FROM HERE
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <TypingText text={"HELLO, WORK FROM HERE. YOU ARE READY TO GO! "} />
     </main>
   );
 }
---
+++ src/components/TypingText.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+interface TypingTextProps {
+  text: string;
+  speed?: number;
+}
+
+const TypingText: React.FC<TypingTextProps> = ({ text, speed = 100 }) => {
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      setDisplayedText((prev) => prev + text[index]);
+      index++;
+      if (index === text.length) {
+        clearInterval(intervalId);
+      }
+    }, speed);
+
+    return () => clearInterval(intervalId);
+  }, [text, speed]);
+
+  return <p>{displayedText}</p>;
+};
+
+export default TypingText;
