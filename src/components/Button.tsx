4f9c4694642b5433e81751499f20418d6678bb68
minseobshim <mshim@blast-team.com>
2024-09-05 00:17:08 +0900

    feat: Button 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+interface ButtonProps {
+  content: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ content }) => {
+  return (
+    <div className="cursor-pointer border-2 border-white px-8 py-4 w-1/6 flex justify-center items-center">
+      {content}
+    </div>
+  );
+};
+
+export default Button;
