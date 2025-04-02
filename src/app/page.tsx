3e2e4f2fe20d9783412bf7f6fbe1b22ec5ff1816
minseobshim <mshim@blast-team.com>
2025-04-02 22:16:59 +0900

    refactor: TypingText 별도 관리를 위해 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import TypingText from "@/components/TypingText";
-import Button from "@/components/Button";
+import TypingText from "@/shared/components/TypingText";
+import Button from "@/shared/components/Button";
+import { TYPE_TEXT } from "@/shared/const/text";
 
 export default function Home() {
   return (
     <main className="mx-auto border-2 border-white w-full max-w-screen-xl mt-8 px-12 pt-6 min-h-[95dvh] flex flex-col">
       <div className={"w-full flex flex-col flex-1 min-h-0 justify-between"}>
         <div className={"border-2 border-gray-500 w-full p-4 min-h-40"}>
-          <TypingText
-            text={
-              "안녕하세요! \n 저는 프론트엔드 개발자 심민섭입니다. \n 저는 힘들 때 벽을 오릅니다. 저는 개발을 참 좋아합니다. 아래 버튼을 통해 궁금하신 부분을 확인해보세요."
-            }
-            speed={50}
-          />
+          <TypingText text={TYPE_TEXT} speed={50} />
         </div>
         <div className={"w-full flex gap-4 py-4"}>
           <Button content={"About Me"} />
--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ content }) => {
   return (
-    <div className="cursor-pointer border-2 border-white px-8 py-4 w-1/6 flex justify-center items-center">
+    <div className="cursor-pointer border-2 border-white px-8 py-4 w-1/6 flex justify-center items-center flex-1">
       {content}
     </div>
   );
