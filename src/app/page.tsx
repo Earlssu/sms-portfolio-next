236475abb04087e19e9fa5c4fb0d4ae4163975bb
minseobshim <mshim@blast-team.com>
2025-04-17 15:35:19 +0900

    feat: 소개 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,16 +13,19 @@ export default function Home() {
   const navigateTo = (page: string) => {
     switch (page) {
       case "aboutMe":
-        return router.push("aboutMe");
+        return router.push("/aboutMe");
       case "career":
-        return router.push("career");
+        return router.push("/career");
       case "skills":
-        return router.push("skills");
+        return router.push("/skills");
       case "contact":
-        return router.push("contact");
+        return router.push("/contact");
     }
   };
 
+  const hero = t("hero");
+  const heroDesc01 = t("heroDesc01");
+  const heroDesc02 = t("heroDesc02");
   const typeText = t("typeText");
   const aboutMe = t("about");
   const career = t("career");
@@ -30,16 +33,23 @@ export default function Home() {
   const contact = t("contact");
 
   return (
-    <main className="mx-auto border-2 border-white w-full max-w-screen-xl mt-8 px-12 pt-6 min-h-[90dvh] flex flex-col">
-      <div className={"w-full flex flex-col flex-1 min-h-0 justify-between"}>
-        <div className={"border-2 border-gray-500 w-full p-4 min-h-40"}>
-          <TypingText text={typeText} speed={25} />
+    <main className="mx-auto border-2 border-white w-full max-w-screen-xl mt-8 px-10 py-6 min-h-[90dvh] flex flex-col">
+      <div className={"w-full flex flex-col flex-1 min-h-0"}>
+        <div>
+          <h1 className="text-3xl font-bold">{hero}</h1>
+          <p className="mt-2 text-xl text-amber-400">
+            {heroDesc01} <br />
+            {heroDesc02}
+          </p>
+          <div className={"w-full flex gap-4 py-4"}>
+            <Button content={aboutMe} onClick={() => navigateTo("aboutMe")} />
+            <Button content={career} onClick={() => navigateTo("career")} />
+            <Button content={skills} onClick={() => navigateTo("skills")} />
+            <Button content={contact} onClick={() => navigateTo("contact")} />
+          </div>
         </div>
-        <div className={"w-full flex gap-4 py-4"}>
-          <Button content={aboutMe} onClick={() => navigateTo("aboutMe")} />
-          <Button content={career} onClick={() => navigateTo("career")} />
-          <Button content={skills} onClick={() => navigateTo("skills")} />
-          <Button content={contact} onClick={() => navigateTo("contact")} />
+        <div className={"border-2 border-gray-500 w-full p-4 min-h-40 flex-1"}>
+          <TypingText text={typeText} speed={25} />
         </div>
       </div>
     </main>
