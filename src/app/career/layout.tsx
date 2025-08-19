fbe7c25324ced6b09fadc1999ad8f73ae68fa354
minseobshim <mshim@blast-team.com>
2025-08-20 00:01:08 +0900

    Fix: hydration 오류 수정

--- src/app/career/layout.tsx
+++ src/app/career/layout.tsx
@@ -1,15 +1,13 @@
 // src/app/career/layout.tsx
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import I18nProvider from "@/shared/components/I18nProvider";
-import CommonHeader from "@/shared/components/CommonHeader";
-import NavButtons from "@/shared/components/NavButtons";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Fragment } from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Career Page",
-  description: "Career only",
+  title: 'Career Page',
+  description: 'Career only',
 };
 
 export default function CareerLayout({
@@ -17,19 +15,5 @@ export default function CareerLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="ko">
-      <body className={inter.className}>
-        <I18nProvider>
-          <header>
-            <CommonHeader />
-            {/* TODO: 적절한 네비게이션 생성 시 해당 컴포넌트는 삭제 */}
-            <NavButtons />
-          </header>
-          {/* NavButtons, container div 등 제외 */}
-          <main>{children}</main>
-        </I18nProvider>
-      </body>
-    </html>
-  );
+  return <Fragment>{children}</Fragment>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client";
+'use client';
 
-import TypingText from "@/shared/components/TypingText";
-import { useCommonTranslations } from "@/shared/hooks/useCommonTranslations";
-import { Fragment } from "react";
+import TypingText from '@/shared/components/TypingText';
+import { useCommonTranslations } from '@/shared/hooks/useCommonTranslations';
 
 export default function Home() {
   const { hero, heroDesc01, heroDesc02, typeText } = useCommonTranslations();
 
   return (
-    <Fragment>
+    <div className="mx-auto border-2 border-white w-full max-w-screen-xl mt-8 px-10 py-6 min-h-[90dvh] flex flex-col">
       <div>
         <h1 className="text-3xl font-bold">{hero}</h1>
         <p className="mt-2 text-xl text-amber-400">
@@ -19,6 +18,6 @@ export default function Home() {
       <div className="border-2 border-gray-500 w-full p-4 min-h-40 flex-1">
         <TypingText text={typeText} speed={25} />
       </div>
-    </Fragment>
+    </div>
   );
 }
