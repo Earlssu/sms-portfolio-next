e3350a2630b70ac195fa72b059ae888a873a0f1b
minseobshim <mshim@blast-team.com>
2025-08-22 00:00:52 +0900

    Style: Button 컴포넌트 스타일 수정

--- src/shared/components/NavButtons.tsx
+++ src/shared/components/NavButtons.tsx
@@ -1,36 +1,15 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import Button from "./Button";
-import { useRouter } from "next/navigation";
-import { NAV_ITEMS } from "@/shared/const/navButtonUrls";
-import { useCommonTranslations } from "@/shared/hooks/useCommonTranslations";
+import React from 'react';
+import Button from './Button';
+import { useRouter } from 'next/navigation';
+import { NAV_ITEMS } from '@/shared/const/navButtonUrls';
+import { useCommonTranslations } from '@/shared/hooks/useCommonTranslations';
 
 const NavButtons: React.FC = () => {
   const router = useRouter();
   const { career, skills, contact } = useCommonTranslations();
   const labels = { career, skills, contact };
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return (
-      <div className="w-full flex gap-4 py-4">
-        {NAV_ITEMS.map((item, idx) => (
-          <button
-            key={`btn_placeholder_${idx}`}
-            className="hover:bg-red-300 border-2 border-white p-4 flex-1"
-            disabled
-          >
-            <span className="text-center">{item.key}</span>
-          </button>
-        ))}
-      </div>
-    );
-  }
 
   return (
     <div className="w-full flex gap-4 py-4">
