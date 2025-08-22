bcad862b8cfabca34e17a5afa259b5e9b056c2a7
minseobshim <mshim@blast-team.com>
2025-08-22 21:25:30 +0900

    Misc: 코드 정리

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,21 +1,14 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import CustomCarousel from '@/app/career/components/CustomCarousel';
 import './styles.css';
 
 const Career = () => {
   const cursorRef = useRef<HTMLDivElement>(null);
   const cursor2Ref = useRef<HTMLDivElement>(null);
-  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (!isMounted) return;
-
     const handleMouseMove = (e: MouseEvent) => {
       if (cursorRef.current) {
         cursorRef.current.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
@@ -52,7 +45,7 @@ const Career = () => {
       document.removeEventListener('mouseenter', handleMouseEnter);
       document.removeEventListener('mouseleave', handleMouseLeave);
     };
-  }, [isMounted]);
+  }, []);
 
   return (
     <div style={{ cursor: 'none' }}>
@@ -60,12 +53,8 @@ const Career = () => {
 
       <CustomCarousel />
 
-      {isMounted && (
-        <>
-          <div ref={cursorRef} className="cursor" />
-          <div ref={cursor2Ref} className="cursor cursor2" />
-        </>
-      )}
+      <div ref={cursorRef} className="cursor" />
+      <div ref={cursor2Ref} className="cursor cursor2" />
     </div>
   );
 };
