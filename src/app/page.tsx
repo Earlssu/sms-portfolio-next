b13955b663461ac867c80c84b2131c1a6085b626
minseobshim <mshim@blast-team.com>
2025-08-25 17:29:18 +0900

    Feat: animate.css 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import TypingText from '@/shared/components/TypingText';
 import { useCommonTranslations } from '@/shared/hooks/useCommonTranslations';
+import 'animate.css';
 
 export default function Home() {
   const { hero, heroDesc01, heroDesc02, typeText } = useCommonTranslations();
@@ -9,14 +10,16 @@ export default function Home() {
   return (
     <div className="mx-auto border-2 border-white w-full max-w-screen-xl mt-8 px-10 py-6 min-h-[90dvh] flex flex-col gap-4">
       <div className={'flex flex-col gap-2'}>
-        <h1 className="text-3xl font-bold">{hero}</h1>
-        <p className="text-xl text-amber-400">
+        <h1 className="text-7xl font-bold">
+          <TypingText text={hero} speed={50} />
+        </h1>
+        <p className="text-xl text-amber-400 animate__animated animate__flipInX animate__delay-2s">
           {heroDesc01} <br />
           {heroDesc02}
         </p>
       </div>
       <div className="border-2 border-gray-500 w-full p-4 min-h-40 flex-1">
-        <TypingText text={typeText} speed={25} />
+        {/*<TypingText text={typeText} speed={25} />*/}
       </div>
     </div>
   );
