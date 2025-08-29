bbe1a7b166090d50668f6b588b67af77dcdf1f9d
minseobshim <mshim@blast-team.com>
2025-08-30 01:19:10 +0900

    Fix: hover 등 글로벌 스타일 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary)',
+          hover: 'var(--primary-hover)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          hover: 'var(--secondary-hover)',
+        },
+        tertiary: {
+          DEFAULT: 'var(--tertiary)',
+          hover: 'var(--tertiary-hover)',
+        },
+        quaternary: {
+          DEFAULT: 'var(--quaternary)',
+          hover: 'var(--quaternary-hover)',
+        },
+        card: {
+          DEFAULT: 'var(--card-bg)',
+          border: 'var(--card-border)',
+          ring: 'var(--card-ring)',
+          hover: 'var(--card-hover)',
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
