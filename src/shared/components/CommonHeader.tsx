0c9e8882550a9289c419043eecf2e6b957928c79
minseobshim <mshim@blast-team.com>
2025-09-05 11:56:59 +0900

    Feat: Header 노션 이력서 링크 추가

--- src/shared/components/CommonHeader.tsx
+++ src/shared/components/CommonHeader.tsx
@@ -1,10 +1,21 @@
 'use client';
 
 import LanguageToggle from '@/shared/components/LanguageToggle';
+import { NotionIcon } from '@/shared/components/icons';
 
 const CommonHeader = () => {
   return (
-    <div className="fixed right-0 z-20">
+    <div className="fixed right-0 z-20 flex gap-4 px-4 pt-2">
+      <a
+        href={
+          'https://fire-warrior-ca2.notion.site/MinSeob-Shim-6004fbc72ef74bc994cea07bf1e11f7e?source=copy_link'
+        }
+        target={'_blank'}
+        className={'flex items-center gap-2'}
+      >
+        <NotionIcon />
+        <p className={'text-sm'}>이력서 보기</p>
+      </a>
       <LanguageToggle />
     </div>
   );
