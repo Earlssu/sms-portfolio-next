d10de66d675145e6389edd79a219f87ea36fde69
minseobshim <mshim@blast-team.com>
2025-09-07 05:38:39 +0900

    Fix: NavButtons 키 문제 해결

--- src/shared/components/NavButtons.tsx
+++ src/shared/components/NavButtons.tsx
@@ -1,22 +1,21 @@
 'use client';
 
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Button from './Button';
 import { useRouter } from 'next/navigation';
 import { NAV_ITEMS } from '@/shared/const/navButtonUrls';
-import { useCommonTranslations } from '@/shared/hooks/useCommonTranslations';
 
 const NavButtons: React.FC = () => {
   const router = useRouter();
-  const { career, skills, contact, home } = useCommonTranslations();
-  const labels = { career, skills, contact, home };
+  const { t } = useTranslation();
 
   return (
     <div className="fixed right-4 top-14 flex flex-col gap-4 z-50">
       {NAV_ITEMS.map((item, idx) => (
         <Button
           key={`btn_${idx}`}
-          content={labels[item.key as keyof typeof labels]}
+          content={t(item.key)}
           onClick={() => router.push(`/${item.url}`)}
         />
       ))}
